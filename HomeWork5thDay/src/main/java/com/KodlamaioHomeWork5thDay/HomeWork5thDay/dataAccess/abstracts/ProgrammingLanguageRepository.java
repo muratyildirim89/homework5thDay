d61bce18f1fb164e63e9e47f1dc07b09afd886e1
Mirat YILDIRIM <77025193+muratyildirim89@users.noreply.github.com>
2022-11-18 14:49:44 +0300

    Delete HomeWork5thDay directory

--- HomeWork5thDay/src/main/java/com/KodlamaioHomeWork5thDay/HomeWork5thDay/dataAccess/abstracts/ProgrammingLanguageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.KodlamaioHomeWork5thDay.HomeWork5thDay.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.KodlamaioHomeWork5thDay.HomeWork5thDay.entities.concretes.ProgrammingLanguage;
-
-public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Integer>{
-
-}
